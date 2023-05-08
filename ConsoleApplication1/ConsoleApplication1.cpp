acfd2c65d5343c8638dbad683d91d9718d114c00
Hyeon Jo Lee <gogreen97@naver.com>
2023-05-08 22:44:50 +0900

    이현조: 배열, 함수

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+//1. 라이브러리: 미리 만들어진 코드들이 모인 곳. 
+
+int main() //2. main 함수
+{ // 함수의 시작
+    std::cout << "Hello World!\n";
+
+    return 0;
+} // 함수의 종료 
+
---
+++ level0(0503)/homeworkpad.cpp
@@ -0,0 +1,19 @@
+﻿// 숙제 푸는 패드. 
+
+//9번 풀이
+#include <iostream>
+int main()
+{
+	int arr[4] = {};
+	int a = 0;
+	int b = 0;
+	std::cin >> a >> b;
+
+	arr[0] = a;
+	arr[2] = b;
+
+	// std::cout << arr;
+	// arr 출력 결과: 0000000F5E6FF928
+
+	std::cout << arr[0] << arr[1] << arr[2] << arr[3];
+}
