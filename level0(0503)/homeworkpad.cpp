9266d794a7adc256e3faa5e7f9ba0b95ca85fbcc
Hyeon Jo Lee <gogreen97@naver.com>
2023-05-16 17:40:56 +0900

    이현조: 0516 문자열, 배열포인터

--- level0(0503)/homeworkpad.cpp
+++ level0(0503)/homeworkpad.cpp
@@ -1,2 +1,46 @@
-﻿// 숙제 패드 
-// 6번 풀이
+﻿// 7번
+#include <iostream>
+
+int arr[7][5] =
+{
+	{1,},
+	{1,0,1,},
+	{1,1,0,1,},
+	{1,0,1,},
+	{0,1,0,0,1},
+	{0,0,0,1,},
+	{1,1,}
+};
+
+int input()
+{
+	int num = 0;
+	std::cin >> num;
+	return num; 
+}
+
+int process()
+{
+	int count = 0; 
+	for (int x = 0; x < 5; x++)
+	{
+		if (arr[0][x] == 1)
+		{
+			count++; 
+		}
+	}
+
+	return count; 
+}
+
+output()
+{
+	std::cout << ;
+}
+
+int main()
+{
+	input();
+	process();
+	output();
+}
