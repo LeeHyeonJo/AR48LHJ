8b363e2e1809eef69b2be08b380ce6827b90a8ed
Hyeon Jo Lee <gogreen97@naver.com>
2023-05-19 20:32:39 +0900

    이현조: 0519 for문 증감식 복습

--- level0(0503)/homeworkpad.cpp
+++ level0(0503)/homeworkpad.cpp
@@ -1,19 +1,6 @@
-﻿//**선택 정렬의 사용법. (오름차순 정리)
+﻿// 2번
 #include <iostream>
 int main()
 {
-	int arr[6] = {5,3,6,1,2,8};
 
-	for (int y = 0; y < 6; y++)
-	{
-		for (int x = y + 1; x < 6; x++) // x는 y의 다음 값이므로 y+1임에 주의!! 
-		{
-			if (arr[y] > arr[x])
-			{
-				int temp = arr[y];
-				arr[y] = arr[x];
-				arr[x] = temp;
-			}
-		}
-	}
 }
