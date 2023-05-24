7b2da915f940b4e32a167b8cb4a4ccf365d3ccdf
Hyeon Jo Lee <gogreen97@naver.com>
2023-05-24 17:48:34 +0900

    이현조: 0524 함수(템플릿, 함수 오버로딩), 클래스(바이트패팅, 생성자 용도:구조체의 멤버변수초기화)

--- level0(0503)/homeworkpad.cpp
+++ level0(0503)/homeworkpad.cpp
@@ -1,22 +1,17 @@
-﻿// 재귀함수 모법담안
+﻿//4번
 #include <iostream>
-
-int arr[5] = { 5,7,1,2,3 };
-
-void wow(int level)
-{
-	if (level == 5)
-	{
-		return; 
-	}
-	std::cout << arr[level]; 
-	wow(level + 1);
-	// return 받고 돌아오는 곳 
-	std::cout << arr[level]; 
-}
-
 int main()
 {
-	wow(0);
-	std::cout << "재귀함수가 종료되었습니다."; 
+	int arrup[5] = {};
+	int arrdown[5] = {};
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cin >> arrup[i];
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cin >> arrdown[i];
+	}
 }
