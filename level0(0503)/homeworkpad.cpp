a8b98a6043a0333c22910656f08480df2eb27e36
Hyeon Jo Lee <gogreen97@naver.com>
2023-05-26 17:25:33 +0900

    이현조:0526 재귀함수응용(경우의수), 삼차원 배열

--- level0(0503)/homeworkpad.cpp
+++ level0(0503)/homeworkpad.cpp
@@ -1,21 +1,28 @@
-﻿#include <iostream>
-// 숙제: 집에서 이거 그림 꼭 다시 그려보기 !! 
-void kfc(int level)
-{
-
-	if (level == 2)
-	{
-		return; 
-	}
-
-	kfc(level + 1);
-	kfc(level + 1);
-	kfc(level + 1);
-	std::cout << level; 
-	// 작동이 어떻게 그려지는지 직접 손으로 그려가면서 확인해보기 
-}
+﻿//3번
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	kfc(0);
+	char map[3][5] =
+	{
+		"ABGK",
+		"TTAB",
+		"ACCD"
+	};
+
+	char pattern[2][3] = {};
+
+	for (int i = 0; i < 2; i++)
+	{
+		for (int x = 0; x < 2; x++)
+		{
+			cin >> pattern[i][x]; 
+		}
+	}
+
+
+
+
+
 }
