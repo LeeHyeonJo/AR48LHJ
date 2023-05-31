da375241a92e715b8bf4bc69faa5120069498e99
Hyeon Jo Lee <gogreen97@naver.com>
2023-05-31 17:48:47 +0900

    이현조: 0531 node 링크드 리스트 응용

--- level0(0503)/homeworkpad.cpp
+++ level0(0503)/homeworkpad.cpp
@@ -1,35 +1,45 @@
-﻿// 중복을 허용하지 않는 뽑기 (전체 3개중에 2개를 중복을 허용하지 않고 뽑을 때) 
+﻿//2번
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-char path[8] = {}; // 왼쪽으로 가면 a, 오른쪽으로 가면 b. 
-int visited[10] = {}; 
+int counting = 0;
+char arr[4] = {}; // 경로를 저장할 배열 
 
-void kfc(int level)
+void test(int level, char _spell[5])
 {
-	if (level == 2) // 레벨이 2일떄 (= 전체에서 뽑을 카드의 수) 
+	if (level == 4) // 4글자를 전부 조합 (깊이 4까지)
 	{
-		cout << path << endl; 
-		return; 
+		// 레벨 4에 도착했을때 카운트
+		counting++;
+		return;
 	}
 
-	for (int i = 0; i < 3; i++) // i = 가지의 수 (=총 카드의 수) 
+
+	// 4글자를 입력받음 (가지 4개)
+	for (int i = 0; i < 4; i++)
 	{
-		if(visited[i]==0) // 방문하지 않았을때만 진행 
-		{ 
-			visited[i] = 1; // 들어가기 직전에 진입 기록
-			path[level] = 'a' + i; // 들어가기 직전에 경로 기록  a,b,c 
-			kfc(level + 1);
-			path[level] = 0; // 돌아간 후 기록 지우기. 
-			visited[i] = 0; // 돌아간 후 기록 지우기. 
+		//B,T가 붙어있을때는, 다음 반복문으로 넘어간다. 
+		//따라서 continue가 옳다. 
+		if ((_spell[level] == 'B' && _spell[i] == 'T') || (_spell[level] == 'T' && _spell[i] == 'B'))
+		{ // leve1>0, level-1 이라는 조건이 붙어야함. 
+			continue;
 		}
+
+		arr[level] = _spell[i]; // 진입 전, arr에 경로 저장 
+		test(level + 1, _spell); // 다음으로 내려갈 수 있게 lv+1
+		arr[level] = 0;
 	}
-	// 원리: 들어가기 전에 경로를 기록. 나오면서(=이전 함수로 돌아가서) 지움. 
+
 }
 
 int main()
- {
-	kfc(0);
+{
+	char spell[5] = {};
+	cin >> spell;
 
+	// 네 글자를 조합해서 나올 수 있는
+	// 모든 경우의 수를 출력.
+
+	test(0, spell);
+	cout << counting;
 }
-
