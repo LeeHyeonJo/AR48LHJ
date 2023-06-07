501d3c0382f72236b185d26a39478f1f8a21fc21
hyeonjo <gogreen97@naver.com>
2023-06-07 12:02:58 +0900

    이현조: 0607 복습

--- 21.5_Linkedlist(fx)/21.5_Linkedlist(fx!!).cpp
+++ 21.5_Linkedlist(fx)/21.5_Linkedlist(fx!!).cpp
@@ -1,7 +1,5 @@
 ﻿// 21.5_LinkedList.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-// 노드2: 가장 끝에 노드 추가(하단 꼬리부) 2가지 방법 학습하기 
-// 0601: 전면부에 노드 추가하는 법 학습 
-
+// 노드를 앞에 추가하기 (푸시 프론트) / 노드를 뒤에 추가하기 (푸시 백) 
 #include <iostream>
 using namespace std;
 
@@ -37,8 +35,23 @@ Node* tail = nullptr;
 // >> 이것을 AddNode 함수로 구현. 
 // >> 목적: main 에서 AddNode(data) 를 선언시, heap에 노드 1개를 동적할당으로 생성하고, 그 노드의 멤버 변수에 입력받은 data값을 넣어준다. 
 
+// ** 노드 추가는 1. 제일 앞에 / 2. 제일 뒤에 할 수 있다.
+// Addnode => push_back으로 이름 변경. 
 
-void AddNode(char data)
+//** 1. 제일 앞에 노드 추가하는 법(푸쉬 프론트) 
+void push_front(char data)
+{
+	Node* temp = new Node(); // 새로운 리모컨을 만든 후, 거기에 뉴 노드를 생성. 리턴되는 주소를 연결해준다. 
+	temp->ch = data; // 뉴 노드에 temp로 접근해 값을 넣어준다 
+	temp->pNext = head; // ** 중요! 뉴 노드는 n1 앞에 연결되어야 함. pNext는 n1으로 가는 주소를 가져야 함.
+						//이 주소는 head에 있으므로 head값을 대입하면 된다. 
+
+	// 그 다음!! head는 항상 앞의 값을 가르켜야 한다. n1 앞에 뉴노드가 들어왔으므로 head도 뉴 노드를 가르켜야 한다. 
+	head = temp; // 뉴 노드의 주소를 갖고있는건 temp이므로 그 값을 head에 넣어준다. 
+}
+
+//** 2. 가장 뒤에 노드 추가하는 법(푸쉬백: 이름만 바꾼거임) 
+void push_back(char data)
 {
 	if (head == nullptr)
 	{
@@ -98,38 +111,62 @@ void AddNode(char data)
 	}
 }
 
-// ******addnode는 가장 뒤에 노드 추가 (푸쉬백)*******
-// ******가장 앞에 노드 추가하는거 필기 복붙 (푸쉬프론트) ******
-
-// 0602 노드 지워주는 법 *****필기 복붙하기**********
-void deletenode(char data)
+// ** 노드 지우는 방법 (new - delete)
+// 이유: 동적할당한 메모리는 지워주지 않으면 프로그램이 종료될 때까지 계속 남아있다.
+// 이 낭비를 막기 위해서, 동적 할당한 메모리는 사용 후 항상 지워주어야 한다. (반납하는 개념) 
+ 
+void DeleteNode(char data) // 뜻: 들어온 데이터에 해당하는 노드를 지운다. 
 {
-	Node* pNode = head; // 새로운 리모컨 추가 . for문에서 i 역할 (노드 돌면서 nullptr 찾는 역할) 
+	Node* pNode = head; // 새로운 리모컨 추가 . for문에서 i 역할 (노드 돌면서 data가 있는 노드를 찾는 역할)  
 	Node* prevNode = head; // 새로운 리모컨 추가 (목적: 지워지는 노드의 "앞" 노드 기억하는 목적) 
 
-	/// 지워야 하는 노드가 제일 앞일떄  *****필기 복붙하기******
-	if (pNode->ch == data) // 노드가 가리킨 것이, 헤드와 동일하다면(직접 해보기) 
+	/// 1. 지워야 하는 노드가 제일 앞일때 
+	if (pNode->ch == data) // 노드가 가리킨 것이, 헤드와 동일하다면(=들어온 데이터 값이 n1에 있는지 여부 체크용) 
 	{
 		head = pNode->pNext; // 헤드를 n1에서 n2로 연결시켜버린다. 
 
-		// 필기 복붙하기 
+		delete pNode; // 새로운 리모컨을 지워준다(반납) 
+		pNode = nullptr; // 지운 후 nullptr값을 넣어준다. (반납 후 null값 넣는것까지가 세트) 
 
 		return; 
 	}
-
-	for(//pNode가 nullptr을 가리키지 않을떄만 돌기 ; 증감식은 pNode = pNode->next)
+	/// 2. 지워야 하는 노드가 중간~끝에 있을 때
+	for(/*pNode의 정의는 이미 위에 해서 생략 가능!*/; pNode->pNext != nullptr;    pNode = pNode->pNext)
+		//pNode가 nullptr을 가리키지 않을떄만 돌기 ; 증감식은 pNode = pNode->next :다음 노드로 이동)
+	{
+		if (pNode->ch == data) // 노드가 가리킨 값에 지워야 할 값이 들어있으면
+			// 이 조건을 충족할떄 pnode는 data 노드에, prev는 바로 앞 노드에 위치하기 된다. (for문 돈 결과) 
 		{
-			// 필기 복붙하기 
+			prevNode->pNext = pNode->pNext; //A 노드 바로 앞에 연결된 prevnode를 A뒤의 노드와 연결시켜주는 과정 
+			//A노드로 가는 값 //B노드로 가는 값   // ㅁ -> A - > B  ~~~~ ㅁ->B 이렇게 
+
+			delete pNode; // data가 들어있는 노드를찾던 리모컨을 반납 
+			pNode = nullptr; // 그 노드값청소 
+
+			delete prevNode; // 리모컨을 반납 
+			prevNode = nullptr; // 그 노드값청소 
+
+			break; 
 		}
+
+		prevNode = pNode; 
+	}
 
 
 }
 
 int main()
 {   // 예시: 새로운 노드를 추가하고, 거기에  data 값을 C, A, D를 넣어주려고 한다. (노드 1개당 하나씩) 
-	AddNode('C');
-	AddNode('A');
-	AddNode('D');
+	// 1. 뒤에 노드 추가 
+	push_back('C'); 
+	push_back('A');
+	push_back('D');
+
+	//2. 앞에 노드 추가 
+	push_front('F');
+
+	// 3. 노드 삭제 
+	DeleteNode('A');
 
 	return 0;
 }
---
+++ 22.5_stack(구현!)/22.5_stack(구현!).cpp
@@ -0,0 +1,108 @@
+﻿// 22.5_stack(구현).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// 자료구조: 스텍 - 접시쌓기 (들어오는 순서대로 넣어주고, 가장 나중에 들어온거부터 빠지는 방식) 
+// 문제를 풀기 위해 클래스를 설계하는 연습을 해야 함. 
+
+// 하는 것: 생성자에서 구조체의 멤버변수를 초기화.
+// 1,2,3 의 값을 PUSH 함수로  넣어주고. TOP 함수로 가장 위의 값을불러냄. 그 후 pop 함수로 가장 윗값을 지우기
+
+// **스택을 링크드 리스트로 구현한 예시. 
+
+#include <iostream>
+using namespace std;
+
+// ** 스택을 사용해보자 **  
+// 노드(데이터 저장 덩어리) 를 stack 형식으로 구현한 것 
+
+struct stack
+{
+public:
+	struct node // 이너 클래스 
+	{
+		node() // 1. node 생성자 호출 (node 내부의 변수를 초기화) 
+			:data(0)
+			, next(nullptr)
+			, prev(nullptr)
+		{
+
+		}
+
+		int data; // 단점: char 형 등 다른 자료형을 구혀하고 싶다면...~ 
+		// char ch; 이렇게 넣어주면 되지만
+
+		// 그럼 push 함수가 int 형, char형 각각 따로따로 추가도게 된다. 
+
+		node* prev;// tail 앞의 노드를 가리키는 포인터 변수 (pop - 가장 윗값을 지우는 함수에서 사용)
+		node* next;
+	}; // 여기까지 앞에서 쭉 해온 노드 구현 
+
+	//2. stack의 생성자 호출 (스택 내부에서 쓸 변수들 초기화) 
+	stack()
+		: mhead(nullptr)
+		, mtail(nullptr)
+	{
+
+	}
+
+	// 3. push 함수: 노드를 스택 방식으로 추가해주는 함수 
+	void push(int num)
+	{
+		if (mhead == nullptr) // 첫 스텍 생성
+		{
+			mhead = new node();
+			mhead->data - num;
+			mhead->prev = nullptr; // 이거 잘 익혀두가 
+			mhead->next = nullptr;
+			mtail = mhead;
+		}
+
+		else // 다음 스텍 생성
+		{
+			mtail->next = new node();
+			mtail->next->data = num;
+
+			// 테일을 끝으로 넘기기 전, 그 테일을 프리브에 연결
+			mtail->next->prev = mtail;
+			mtail->next->next = nullptr; // 마지막 노드에 널값
+
+			// 테일 이동
+			mtail = mtail->next;
+
+
+		}
+	}
+
+	void pop() // 맨 위의 스텍을 지우는 함수 
+	{
+		node* last = mtail; // 새로운 리모컨을 추가.
+
+		mtail = mtail->prev; // 테일은 테일 바로 앞의 값을 가리키는 프리브로 연결
+		mtail->next = nullptr; // 이동 후 n3에연결되어있던 것을 해제 
+
+		delete last; // 리모컨 다 썼으니 할당 해제
+		last = nullptr; // 해제 후 쓰레기값 안 들어가게 null값 넣어주기 
+	}
+
+	int top() // 맨 위의 스택 값을 불러오는 함수 
+	{
+		return mtail->data;
+	}
+
+private:
+	node* mhead;
+	node* mtail; // 스택용 
+};
+
+int main()
+{
+	stack stack1;
+
+	stack1.push(1);
+	stack1.push(2);
+	stack1.push(3);
+
+	int top = stack1.top();
+	stack.pop();
+
+	return 0;
+
+}
