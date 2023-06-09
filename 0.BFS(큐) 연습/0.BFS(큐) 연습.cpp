243f5a997004dd248338e3d319c45b8960055ea7
Hyeon Jo Lee <gogreen97@naver.com>
2023-06-09 19:26:00 +0900

    이현조: 0609 BFS 트리 탐색 복습

---
+++ 0.BFS(큐) 연습/0.BFS(큐) 연습.cpp
@@ -0,0 +1,127 @@
+﻿// 0.BFS(큐) 연습.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// 3번 복습함. 돌아가는 원리, 그려가면서 이해하기. 
+
+#include <iostream>
+#include <queue> // 큐를 사용하기 위함. 
+
+// 이게 큐 구현 원리 
+//template <typename T>
+//class Queue
+//{
+//public:
+//    Queue()
+//        : mDatas{-1, -1,}
+//        , mFront(0)
+//        , mEnd(0)
+//    {
+//
+//    }
+//
+//    void Push(T data)
+//    {
+//        mDatas[mEnd] = data;
+//        mEnd++;
+//    }
+//
+//    T Pop()
+//    {
+//        return mDatas[mFront++];
+//    }
+//
+//private:
+//    T mDatas[256];
+//    int mFront;
+//    int mEnd;
+//};
+
+
+// 그래프 탐색할떄 노드가 사용됬는지
+// 체크해주는 배열
+int used[256] = {};
+
+int bfsMatrix[6][6] =
+{
+	0,1,1,0,0,0,
+	0,0,0,1,1,0,
+	0,0,0,0,0,1,
+	0,0,0,0,0,0,
+	0,0,0,0,0,0,
+	0,0,0,0,0,0,
+};
+// 그래프를 그리고 시작. 
+
+
+struct Node
+{
+	int num;
+	int level;
+};
+
+
+int main()
+{
+	std::queue<Node> queue;
+	queue.push(Node({ 0,0 }));
+
+	while (!queue.empty())
+	{
+		Node p = queue.front();
+		std::cout << p.num << " ";
+
+		for (int i = 0; i < 6; i++)
+		{
+			if (bfsMatrix[p.num][i] == 1
+				&& used[i] == 0)
+			{
+				used[i] = 1;
+				queue.push(Node{ i, p.level + 1 });
+			}
+		}
+		queue.pop();
+	}
+	
+}
+
+//std::queue<Node2> queue;
+// queue<Node2> : 여기까지가 자료형
+// queue뒤의 큐는 변수. n1 같은거. 
+
+///*이진트리 dfs로 푸는 문제*///
+// char binaryTree[256] = "_ABCDEFG";
+// 
+//void dfs(int level)
+//{
+//	if (binaryTree[level] == '\0')
+//	{
+//		return;
+//	}
+//
+//	dfs(level * 2);
+//	dfs(level * 2 + 1);
+//}
+
+//int* p = new int[num2];
+
+
+///*레벨 29의 이진트리를 BFS로 푸는 문제*///
+// int main
+// {
+//std::queue<Node2> queue;
+//queue.push(Node2({ 1,0 }));
+//
+//while (!queue.empty())
+//{
+//	Node2 p = queue.front();
+//	std::cout << binaryTree[p.idx] << " ";
+//
+//	if (binaryTree[p.idx * 2] != '\0')
+//	{
+//		queue.push(Node2{ p.idx * 2 , p.level + 1 });
+//	}
+//	if (binaryTree[p.idx * 2 + 1] != '\0')
+//	{
+//		queue.push(Node2{ p.idx * 2 + 1 , p.level + 1 });
+//	}
+//
+//	queue.pop();
+//}
--- level0(0503)/homeworkpad.cpp
+++ level0(0503)/homeworkpad.cpp
@@ -1,20 +1,19 @@
 ﻿// 7번
 #include <iostream>
 using namespace std;
-int btree[8] = {0,3,7,10,5,6,3,6}; 
-	   		//  0 1 2  3 4 5 6 7
+int btree[8] = { 0,3,7,10,5,6,3,6 };
+//  0 1 2  3 4 5 6 7
 int visited[8] = {}; // 방문 여부 체크 
 int path[8] = {};
 
 // 이차원 트리에 한해서 일차원 배열로 표현할 수 있다. 
 // 부모 - 자식 공식을 사용하기 위해 인덱스 1부터 값 넣어줌. 
 
-void dfs (int level, int now) // 레벨과, 초기값(변동) 
+void dfs(int level, int now) // 레벨과, 초기값(변동) 
 {
-	if (level == 2)
-	{
-		return; 
-	}
+	// 1. 레벨 2일때 리턴되면 안되지.
+	// 2. 출력이 for문 위에서 
+	// 3. 짝수 체크는 path 에서. 
 
 	for (int i = 1; i < 8; i++)
 	{
@@ -27,19 +26,19 @@ void dfs (int level, int now) // 레벨과, 초기값(변동)
 		if (btree[now * 2] != 0 && visited[i] == 0)
 		{
 			visited[i] = 1; // 첫값 때문에 두번째 자리부터 
-			path[level+1] = btree[now*2]; // 첫값 때문에 두번째 자리부터 
+			path[level + 1] = btree[now * 2]; // 첫값 때문에 두번째 자리부터 
 
 			if (btree[now * 2] % 2 == 0) // 짝수라면 
 			{
 				// 멈추고 출력 
 				for (int i = 0; path[i] != 0; i++)
 				{
-					cout << path[i] << ' '; 
+					cout << path[i] << ' ';
 				}
 			}
 
-			dfs(level + 1, i*2); 
-			path[level+1] = 0; 
+			dfs(level + 1, i * 2);
+			path[level + 1] = 0;
 		}
 
 	}
@@ -53,5 +52,5 @@ int main()
 	//	cin >> btree[i]; 
 	//}
 
-	dfs(0, 1); 
+	dfs(0, 1);
 }
