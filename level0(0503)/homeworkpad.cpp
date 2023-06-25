849a7c49a463051a86b86fe17f2721819ecec477
hyeonjo <gogreen97@naver.com>
2023-06-26 01:05:54 +0900

    Revert "이현조: 0616 레벨 18~19 복습"
    This reverts commit 1fd308d84f84f7144eb5f6c3b8659dd6af71a3de.

--- level0(0503)/homeworkpad.cpp
+++ level0(0503)/homeworkpad.cpp
@@ -1,75 +1,35 @@
-﻿// 8번
+﻿// 11번
 #include <iostream>
 using namespace std;
 
-int image[4][4] =
-{
-	1, 5, 5, 4,
-	4, 2, 1, 1,
-	3, 9, 3, 2,
-	4, 5, 9, 1 
-};
-
-int max1 = -1234; 
-
-int newY = 0; 
-int newX = 0; 
-
-int maxY = 0; 
-int maxX = 0; 
-
-int rectSum(int _y, int _x)
-{
-	int sumsum = 0;
-
-	// 2x3 사이즈의 합을 리턴해주는 함수 
-	// 스타트 값에서 2x3을 서치하는 것 
-	for (int y = 0; y < 2; y++)
-	{
-		for (int x = 0; x < 3; x++)
-		{
-			// **이 부분 주의 
-			newY = _y + y; 
-			newX = _x + x; 
-
-			// **2x3 배열이 map 밖으로 나가지 않게 
-			if ((newY >= 0 && newY <= 3) && (newX >= 0 && newX <= 3))
-			{
-				sumsum += image[newY][newX]; 
-			}
-		}
-	}
-	
-	return sumsum; // 총합만 리턴함 
-}
+char arr[11] = ""; 
 
 int main()
 {
-	// 이미지 배열에 입력 
-	//for (int i = 0; i < 4; i++)
-	//{
-	//	for (int k = 0; k < 4; k++)
-	//	{
-	//		cin >> image[i][k]; 
-	//	}
-	//}
+	cin >> arr; 
 
-	// 배열을 돌면서 시작 좌표를 함수로 보냄 
-	for (int y = 0; y < 4; y++)
+	for (int i = 0; i < strlen(arr); i++)
 	{
-		for (int x = 0; x < 4; x++)
+		if (arr[i] == 'G') // 첫 글자가 G라면 
 		{
-			int result = rectSum(y, x); // 여러 값이 리턴될거임 
-
-			// 여기서 합의 최대값을 비교 
-			if (max1 < result) 
+			if (strlen(arr) - i - 1 >= 4)// 뒤에 HOST가 올 공간이 있다면
 			{
-				max1 = result; 
-				maxY = y;
-				maxX = x;
+				// 그 다음 글자들 비교 
+				if (arr[i + 1] == 'H' && arr[i + 2] == 'O' && arr[i + 3] == 'S' && arr[i + 4] == 'T')
+				{
+					cout << "존재";
+				}
+
+				else
+				{
+					cout << "존재하지 않음"; // 글자수는 충분, but 다른 글자가 들어감. 
+				}
+			}
+
+			else
+			{
+				cout << "존재하지 않음"; // 글자수 부족 
 			}
 		}
 	}
-
-	cout << "(" << maxY << "," << maxX << ")"; 
 }
